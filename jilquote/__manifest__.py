31ee3db99599d2bc97b76c25e959f8cef1e8fc8c
webcode <erlang.ug@gmail.com>
2018-02-14 11:11:44 +0300

    Add files via upload

---
+++ jilquote/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'JILINSIGHTS CUSTOM INVOICE MODULE',
+    'summary': 'A module to revamp the look and feel of the JilInsights Invoice',
+    'author': 'Joel Elayu',
+    'category': 'Documentation',
+    'version': '1.0',
+    'depends': ['base','account_accountant'],
+    'data': ['reports/custom_header.xml',
+              'reports/jil_custom_invoice.xml',
+              'reports/jil_custom_quote.xml'],
+    'application': True,
+}
